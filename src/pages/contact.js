9e069cd0385115c16567cb35edf44bb36599929c
Russ Wells <russ@russdigital.com>
2018-07-25 17:56:32 -0700

    sexy awakening privacy policy and tos

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const Contact = () => (
+  <div>
+    <h1>Contact</h1>
+    <p>To contact Sexy Awakening, send and email to sexyawakening@gmail.com</p>
+    <Link to="/">Go back to the homepage</Link>
+  </div>
+)
+
+export default Contact
