4fea94d9e70b30c29930e96f23148bd83a860cb8
Russ Wells <russ@russdigital.com>
2018-08-11 16:19:55 -0700

    editing

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,11 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <p>Sexy Awakening is Earth's first conscious sexuality portal.</p><p>We believe that relationships are a <b>transformational experience</b> and we exist in order to facilitate <b>evolutionary connections</b>. We encourage honest upfront communication and radical self-expression on our platform. Sexy Awakening is a place for and to connect with beings of <b>higher consciousness</b>. We are a sex positive, inclusive space. A space to express, grow, and play with your sexuality, desires, and social ability. We understand sexuality to be fluid, gender as nonbinary, and everyone as sacred.</p>
+    <p>Sexy Awakening is Earth's first conscious sexuality portal.</p>
     <h2>Swipe with Intention</h2>
-    <p>Swipe right for sex, up for romance, left for friendship, and down to pass. If someone swipes on you with the same intention, then voila, you're connected and can <b>begin chatting with a mutual understanding</b>. Enjoy higher consciousness, a culture of consent, and intentional connections on Sexy Awakening.</p>
+    <p>Swipe right for sex, up for romance, left for friendship, and down to pass. If someone swipes on you with the same intention you are connected and can <b>begin chatting with a mutual understanding</b>. Enjoy higher consciousness, a culture of consent, and intentional connections on Sexy Awakening.</p>
+    <h2>About Us</h2>
+    <p>We believe that relationships are a <b>transformational experience</b> and we exist in order to facilitate <b>evolutionary connections</b>. We encourage honest upfront communication and radical self-expression on our platform. Sexy Awakening is a place for and to connect with beings of <b>higher consciousness</b>. We are a sex positive, inclusive space. A space to express, grow, and play with your sexuality, desires, and social ability. We understand sexuality to be fluid, gender as nonbinary, and everyone as sacred.</p>
     <h2>Membership</h2>
     <p>Sexy Awakening is <b>free to join</b>. Unlimited memberships will be available soon for $11 per month. You must be 18 or older to create an account.</p>
     <h2>How to Join</h2>
