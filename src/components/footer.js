32b2f93bbfbed52a80eca840b39625e990e43fe0
Russ Wells <russ@russdigital.com>
2019-06-10 16:24:37 -0700

    copy and footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -15,12 +15,15 @@ const Footer = () => (
         padding: '1.45rem 1.0875rem',
       }}
     >
-      <p style={{color:'white'}}>Sexy Awakening &copy; 2018 | &nbsp;
+      <p style={{color:'white'}}>Sexy Awakening &copy; 2019 | &nbsp;
       <Link to="/guidelines/" style={{color:'white', textDecoration:'none'}}>Guidelines</Link> | &nbsp;
       <Link to="/privacy-policy/" style={{color:'white', textDecoration:'none'}}>Privacy Policy</Link> | &nbsp;
       <Link to="/terms-of-service/" style={{color:'white', textDecoration:'none'}}>Terms of Service</Link> | &nbsp;
       <Link to="/careers/" style={{color:'white', textDecoration:'none'}}>Careers</Link> | &nbsp;
-      <Link to="/contact/" style={{color:'white', textDecoration:'none'}}>Contact</Link>
+      <Link to="/contact/" style={{color:'white', textDecoration:'none'}}>Contact</Link> &nbsp;
+      <br /><br />
+      <a href="https://www.instagram.com/sexyawakening/" style={{color:'white', textDecoration:'none'}}>Instagram</a> | &nbsp;
+      <a href="https://www.facebook.com/SxxyAwakening/" style={{color:'white', textDecoration:'none'}}>Facebook</a> &nbsp;
       </p>
     </div>
   </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,20 +3,20 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <p>Sexy Awakening is Earth's first conscious sexuality and relationship portal. It's best way to meet new romantic interests, lovers, and friends online. Explore conscious sexuality and relationships!</p>
-    <p>Swipe right for sex, up for romance, left for friendship, and down to pass. If someone swipes on you with the same intention you are connected and can <b>begin chatting with a mutual understanding</b>. Enjoy higher consciousness, a culture of consent, and intentional connections on Sexy Awakening.</p>
+    <p>Sexy Awakening is Earth's first conscious sexuality portal. It's best way to meet new romantic interests, lovers, and friends online. Explore conscious sexuality and relationships!</p>
+    <p>Swipe with intention! Swipe right for sex, up for romance, left for friendship, and down to pass. If someone swipes on you with the same intention you are connected and can <b>begin connecting with a mutual understanding</b>. Sexy Awakening cultivates higher consciousness, a culture of consent, and intentional connections.</p>
     <h2>About Us</h2>
-    <p>We believe that relationships are a <b>transformational experience</b> and we exist in order to facilitate <b>evolutionary connections</b>. We encourage honest upfront communication and radical self-expression on our platform. Sexy Awakening is a place for and to connect with beings of <b>higher consciousness</b>. We are a sex positive, inclusive space. A space to express, grow, and play with your sexuality, desires, and social ability. We understand sexuality to be fluid, gender as nonbinary, and everyone as sacred.</p>
+    <p>We believe that relationships are a <b>transformational experience</b> and we exist in order to facilitate <b>evolutionary connections</b>. We encourage honest upfront communication and radical self-expression on our platform. Sexy Awakening is a place for and to connect with beings of <b>higher consciousness</b>. This is a sex positive and inclusive space held for everyone to express, grow, play and connect on. On Sexy Awakening there is space for your sexuality, desires, and abilities in their fullnesses. We understand sexuality to be fluid, gender as nonbinary, and everyone as sacred. Enjoy yourselves.</p>
     <h2>Membership</h2>
-    <p>Sexy Awakening is <b>free to join</b>. Unlimited memberships will be available soon for $11 per month. You must be 18 or older to create an account.</p>
+    <p>Sexy Awakening is <b>free to join</b>. Unlimited memberships will be available for $11 per month. They include unlimited connections and advanced match filters. You must be 18 or older to create an account.</p>
     <h2>How to Join</h2>
-    <p>iPhone users download the app <a href="https://testflight.apple.com/join/A52QNb4b">here</a>. Android users can download the App in the <a href="https://play.google.com/store/apps/details?id=com.DigitalHyperspace.SexyAwakening">Play Store.</a></p>
-    <p>Available for iOS, Android, and soon the web.</p>
+    <p>Relaunching soon for iOS, Android, and the web.</p>
+    {/*<p>iPhone users download the app <a href="https://testflight.apple.com/join/A52QNb4b">here</a>. Android users can download the App in the <a href="https://play.google.com/store/apps/details?id=com.DigitalHyperspace.SexyAwakening">Play Store.</a></p>*/}
     <h2>Conscious Culture</h2>
     <p>Sexy Awakening encourages a culture of immediacy, sexuality, play, exploration, intuition, and transformation, grounded in a deeply rooted sense of autonomy, respect, and consent.</p>
     <p>Please take a look at our <Link to="/guidelines/">guidelines</Link> before connecting.</p>
     <h2>Coming Soon</h2>
-    <p>We are adding more features to make Sexy Awakening even more potent. See what's <Link to="/coming-soon/">brewing</Link>.</p>
+    <p>We are adding more features to make Sexy Awakening even more potent, activating, empowering, revolutionary, and safe. See what's <Link to="/coming-soon/">brewing</Link>.</p>
   </div>
 )
 
